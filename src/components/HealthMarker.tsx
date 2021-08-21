7219b899573fb69cb834202167be10faf155eddc
Vesper Huster <vesper@sarox.com.au>
2021-08-22 01:37:25 +1000

    WIP: Trying to get heatmap going

---
+++ src/components/HealthMarker.tsx
@@ -0,0 +1,47 @@
+import React, {useMemo, useRef, useState} from 'react';
+import {LatLng} from 'leaflet';
+import {Marker, Popup} from 'react-leaflet';
+import {Progress} from 'antd';
+
+export interface HealthMarkerProps {
+    initialPosition: LatLng;
+    pointHealth: number;
+}
+
+const healthColors: {[x: number]: string} = {
+    20: '#dc0000',
+    40: '#dc8400',
+    60: '#dcd100',
+    80: '#acdc00',
+    100: '#3ace00',
+};
+
+function getHealthColor(health: number, healthBound: number = 20): string {
+    if (health < healthBound) {
+        return healthColors[healthBound];
+    }
+    return getHealthColor(health, healthBound + 20);
+}
+
+export const HealthMarker: React.FC<HealthMarkerProps> = ({initialPosition, pointHealth}) => {
+    const [position, setPosition] = useState(initialPosition);
+    const markerRef = useRef<any>(null);
+
+    const friendlyPosition = `(${position.lat.toFixed(3)}, ${position.lng.toFixed(3)})`;
+
+    return (
+        <Marker
+            draggable
+            position={position}
+            ref={markerRef}
+        >
+            <Popup minWidth={140}  >
+                <div className="flex flex-col items-center text-center" >
+                    <Progress type="circle" strokeColor={getHealthColor(pointHealth)} percent={pointHealth} width={80} />
+                    <span className="text-lg">Health</span>
+                    <span>{friendlyPosition}</span>
+                </div>
+            </Popup>
+        </Marker>
+    );
+};
--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,19 +1,34 @@
 import {TileLayer, useMap, useMapEvents} from 'react-leaflet';
-import {LatLng} from 'leaflet';
+import L, {LatLng} from 'leaflet';
+import 'leaflet-webgl-heatmap';
+import {useEffect, useState} from 'react';
+import {HealthMarker} from './HealthMarker';
+import {healthPoints} from '../health-points';
 
 export interface MapProps {
     onMoveEnd: (position: LatLng, zoomLevel: number) => void
 }
 
 export const Map: React.FC<MapProps> = ({onMoveEnd}) => {
+    const [healthMarkers, setHealthMarkers] = useState<JSX.Element[]>([]);
     const map = useMap();
-    map.scrollWheelZoom.enable();
 
     const tileLayerUrl = 'https://api.mapbox.com/styles/v1/thefrogcouncil/cksleyr2s3nyn17pqgm8fg2ew/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoidGhlZnJvZ2NvdW5jaWwiLCJhIjoiY2tzbGVyZmN3MDA0MDJvbWdvbmdoY3RyNCJ9.DTwqWeCHooHnxDwgCJ0sXw';
 
+    useEffect(() => {
+        map.scrollWheelZoom.enable();
+        // @ts-ignore
+        const heatmap = new L.webGLHeatmap({size: 1000});
+        heatmap.setData(healthPoints);
+        map.addLayer(heatmap);
+    });
+
     useMapEvents({
         click(e) {
-            console.log(e.latlng);
+            const health = Math.floor(Math.random() * 100);
+            setHealthMarkers([...healthMarkers, (
+                <HealthMarker initialPosition={e.latlng} pointHealth={health} />
+            )]);
         },
         moveend(e) {
             onMoveEnd(map.getCenter(), map.getZoom());
@@ -26,6 +41,7 @@ export const Map: React.FC<MapProps> = ({onMoveEnd}) => {
                 attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                 url={tileLayerUrl}
             />
+            {healthMarkers}
         </>
     );
 };
